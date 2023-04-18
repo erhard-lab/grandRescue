631b34a8a96919219cb56936b2d65c6c83a0e876
kevin <kevin.berg@uni-wuerzburg.de>
2023-04-18 16:11:27 +0200

    plotEstimates.R updated, decluttered

--- src/resources/plotEstimates.R
+++ src/resources/plotEstimates.R
@@ -7,17 +7,14 @@ library(cowplot)
 t_mapFastq=read.delim("grandslam_t15_noresc/noresc.binomEstimated.tsv")
 t_mapFastq$Condition=factor(as.character(t_mapFastq$Condition),levels=as.character(t_mapFastq$Condition))
 t_mapFastq$Status <- paste(t_mapFastq$Status, "unrescued", sep="_")
-t_mapFastq <- filter(t_mapFastq, Condition != "Spt6+.2p", .preserve = T)
 
 t_resc=read.delim("grandslam_t15_resc/resc.binomEstimated.tsv")
 t_resc$Condition=factor(as.character(t_resc$Condition),levels=as.character(t_resc$Condition))
 t_resc$Status <- paste(t_resc$Status, "rescued", sep="_")
 t_resc$Condition <- str_replace(t_resc$Condition, "_rescued", "")
-t_resc <- filter(t_resc, Condition != "Spt6+.2p", .preserve = T)
 
 t_merge <- rbind(t_mapFastq,t_resc)
 t_merge=t_merge[!grepl("no4sU",t_merge$Condition),]
-t_merge <- filter(t_merge, Condition != "Spt6+.0p", .preserve = T)
 t_merge$conv_old <- NULL
 r_merge <- melt(t_merge[,c(1:2,6)])
 
